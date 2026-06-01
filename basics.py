5e1fee40871e9f231bfbf3bd27dd475233aa8c14
Sai Pavan <saipavan9183@gmail.com>
2026-06-01 18:06:02 +0530

    initial commit

---
+++ basics.py
@@ -0,0 +1,64 @@
+# Student Grade Checker
+# stu_1=int(input())
+# if stu_1 >= 90:
+#     print("A")
+# elif stu_1>=80:
+#     print("B")
+# elif stu_1>=70:
+#     print("C")
+# else:
+#     print("FAIL")
+
+# EVEN OR ODD
+# num = int(input("enter the number: "))
+# if num%2==0:
+#     print("the number is EVEN")
+# else:
+#     print("the number is ODD")
+
+# name repeater
+# name=input("enter your name")
+# for i in range(5):
+#     print(name)
+
+
+# sum from 1 to N
+# n=int(input("enter the number : "))
+# total = 0
+# for i in range(1,n+1):
+#     total += i
+# print("sum =",total)
+
+#
+# str1=input("enter the name : ")
+# count=0
+# for ch in str1:
+#     if ch.lower() in "aeiou":
+#         count+=1
+# print("number of vowels:",count)
+
+# largest number
+# numbers = []
+
+# for i in range(5):
+#     num = int(input("Enter a number: "))
+#     numbers.append(num)
+
+# largest = max(numbers)
+
+# print("Largest number:", largest)
+
+#smallest number
+numbers = []
+
+for i in range(5):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+
+smallest = min(numbers)
+
+print("smallest number:", smallest)
+
+
+
+
